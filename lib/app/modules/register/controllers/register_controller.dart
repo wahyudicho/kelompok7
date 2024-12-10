3b7e52931eeebc848068c592f1382a33e22cedb9
riyandikaibrahim0311 <riyandikaibrahim0311@gmail.com>
2024-12-10 11:47:34 +0000

    ubah register controllers

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -1,23 +1,51 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class RegisterController extends GetxController {
-  //TODO: Implement RegisterController
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  var isTermsAccepted = false.obs;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  void register() {
+    if (!isTermsAccepted.value) {
+      Get.snackbar("Error", "You must accept the terms and conditions");
+      return;
+    }
+
+    if (passwordController.text != confirmPasswordController.text) {
+      Get.snackbar("Error", "Passwords do not match");
+      return;
+    }
+
+    // Perform registration logic here
+    Get.snackbar("Success", "Account created successfully!");
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void signInWithGoogle() {
+    // Google sign-in logic
+  }
+
+  void signInWithApple() {
+    // Apple sign-in logic
+  }
+
+  void signInWithFacebook() {
+    // Facebook sign-in logic
+  }
+
+  void navigateToLogin() {
+    // Navigate to login page
+    Get.toNamed('/login');
   }
 
   @override
   void onClose() {
+    usernameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
